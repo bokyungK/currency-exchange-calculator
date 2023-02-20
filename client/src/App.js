86e835fe8d5c29d1bb13e50f44487a8b515bc4be
freebob2b <freebob2b@gmail.com>
2023-02-20 21:38:48 +0900

    complete assignment

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
+  const [countryState, setCountryState] = useState();
+  const beforeCountry = useRef();
+  const afterCountry = useRef();
+  const before = useRef();
+  const after = useRef();
+
+  useEffect(() => {
+    fetch('https://api.exchangerate.host/latest')
+    .then(res => res.json())
+    .then(data => {
+      const rates = data.rates;
+      const countryArr = [];
+
+      for (const country in rates) {
+          countryArr.push(country);
+      }
+      setCountryState(countryArr);
+    })
+  }, []);
+
+    const handleGetRates = () => {
+      const currentCountry = beforeCountry.current.value;
+
+      fetch(`https://api.exchangerate.host/latest?base=${currentCountry}`)
+      .then(res => res.json())
+      .then(data => data.rates)
+      .then(rates => {
+        const NewCountry = afterCountry.current.value;
+
+        if (currentCountry === NewCountry) {
+          window.alert('같은 나라입니다!');
+          return;
+        }
+
+        if (before.current.value === '') {
+          window.alert('계산할 숫자를 입력해주세요!');
+          return
+        }
+      
+        const newRate = rates[NewCountry];
+        const result = newRate * before.current.value;
+        after.current.value = result;
+      })
+    }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='section'>
+        <input ref={before} className="box" type='number' placeholder='숫자를 입력해주세요!'>
+        </input>
+        <select ref={beforeCountry} className='selectBox'>
+          {
+            countryState ? (
+              countryState.map((name) => {
+                return <option key={name}>{name}</option>
+              })
+            ) : ''
+          }
+        </select>
+      </div>
+      <div className='section'>
+        <input ref={after} className="box" type='number' disabled>
+        </input>
+        <select ref={afterCountry} className='selectBox'>
+          {
+            countryState ? (
+              countryState.map((name) => {
+                return <option key={name}>{name}</option>
+              })
+            ) : ''
+          }
+        </select>
+      </div>
+      <button className='section button' onClick={handleGetRates}>환율 계산</button>
     </div>
   );
 }
